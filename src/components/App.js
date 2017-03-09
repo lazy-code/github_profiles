09f062c735f618450fcadaaae05b9dd4af40d5e7
Lazy Code <fbenalex@gmail.com>
2017-03-09 18:11:03 +0200

    App initial state

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: 'lazy-code',
+      userData: [],
+      userRepos: [],
+      perPage: 5
+    }
+  };
+
   render() {
     return (
       <div>
-        my app
+        {this.state.userName}
       </div>
     );
   }
 }
+
+App.propTypes = {
+  clientId: PropTypes.string,
+  clientSecret: PropTypes.string
+};
+
+App.defaultProps = {
+  clientId: '7d654af83bb87d52dde6',
+  clientSecret: 'cfcde81879b35d85cacffce04dc3d10bcf6d4a1b'
+};
 
 export default App;
